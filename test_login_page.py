d72fd32e569e9982cfd12ab0db1f8e17d54c4340
stepaks675 <catman675to@gmail.com>
2026-08-30 23:35:31 +0300

    enhanced login page

---
+++ test_login_page.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+from .pages.login_page import LoginPage
+
+def test_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
